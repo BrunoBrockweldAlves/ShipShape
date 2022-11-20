55793177749b4a637ad9650a35abff6d91ef876d
Bruno Brockweld Alves <bruno7283@hotmail.com>
2022-11-20 06:15:58 -0300

    Desenvolvimento de forms (tutorial angular)

---
+++ Front/ShipShapeFront/src/app/forms/questions/mock-questions.ts
@@ -0,0 +1,11 @@
+import { Question } from './question';
+
+export const QUESTIONS: Question[] = [
+  { description: 'Pergunta 1', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 2', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 3', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 4', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 5', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 6', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+  { description: 'Pergunta 7', possibilityOne:'', possibilityTwo:'', possibilityThree:'' },
+];
---
+++ Front/ShipShapeFront/src/app/forms/questions/question.ts
@@ -0,0 +1,6 @@
+export interface Question {
+    description: string;
+    possibilityOne: string;
+    possibilityTwo: string;
+    possibilityThree: string;
+  }
